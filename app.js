2796c26d36a96c54128dc6f6676d9fe750870864
Amber Stevens <anani.mowse@gmail.com>
2020-12-18 17:27:43 -0800

    Added event listener for overlay removal

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 //GLOBAL VARIABLES 
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
+const startGame = document.querySelector(".btn__reset");
+startGame.style.cursor = "pointer";
 let missed = 0;
+
+
+//Remove overlay and start game
+
+startGame.addEventListener("click" , () => {
+    const overlay = document.getElementById("overlay");
+    overlay.style.display = "none"
+
+});
